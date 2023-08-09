906cec71f149900042a266954b4323959c542d0f
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-09 22:56:28 +0900

    chakra-uiを使ってみる

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
+import {Button, ChakraProvider} from "@chakra-ui/react";
 
 export default function App() {
   return (
-    <></>
+    <>
+      <ChakraProvider>
+        <Button colorScheme="teal">ボタン</Button>
+      </ChakraProvider>
+    </>
   )
 }
