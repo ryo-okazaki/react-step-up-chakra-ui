3d08f853d6081a37cd0a25bad62142608e1b2fcf
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-13 01:19:38 +0900

    ログインページを表示

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css'
-import {Button, ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider} from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import './App.css';
 import theme from './theme.ts/theme';
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <>
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ChakraProvider>
     </>
   )
