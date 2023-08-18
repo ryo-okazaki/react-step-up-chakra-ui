b9c9ee599b09532e7587dfa19a8f656c867d5f59
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 03:00:22 +0900

    ヘッダーのひな型作成

--- react/src/router/Router.tsx
+++ react/src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { HomeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: FC = memo(() => {
   return (
@@ -15,7 +16,9 @@ export const Router: FC = memo(() => {
         <Switch>
           {HomeRoutes.map((route) => (
             <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}> 
-              {route.children}
+              <HeaderLayout>
+                { route.children }
+              </HeaderLayout>
             </Route>
           ))}
         </Switch>
