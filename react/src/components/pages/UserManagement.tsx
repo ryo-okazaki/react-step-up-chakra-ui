109508d254f673e844b81c8dd84f05e4f4c5ff59
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 18:00:10 +0900

    APIを利用してユーザーを取得

--- react/src/components/pages/UserManagement.tsx
+++ react/src/components/pages/UserManagement.tsx
@@ -1,14 +1,30 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { FC, memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [])
+
   return (
-    <Wrap spacing="30px" p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard imageUrl="https://source.unsplash.com/random" userName="test" fullName="test full name" />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center>
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap spacing="30px" p={{ base: 4, md: 10 }}>
+            {users.map((user) => (
+              <WrapItem key={user.id} mx="auto">
+                <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+              </WrapItem>
+            ))}
+        </Wrap>
+      )}
+    </>
   )
 });
