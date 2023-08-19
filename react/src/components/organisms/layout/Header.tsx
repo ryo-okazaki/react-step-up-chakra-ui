080d81f3d4c8449a94a2e80cb8646d3dc68664d4
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 13:42:45 +0900

    画面遷移の完成

--- react/src/components/organisms/layout/Header.tsx
+++ react/src/components/organisms/layout/Header.tsx
@@ -16,27 +16,39 @@ const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Flex 
-      as="nav" 
-      bg="teal.500" 
-      color="gray.50" 
-      align="center" 
-      justify="space-between" 
-      padding={{ base: 3, md: 5 }}
-    >
+        as="nav" 
+        bg="teal.500" 
+        color="gray.50" 
+        align="center" 
+        justify="space-between" 
+        padding={{ base: 3, md: 5 }}
+      >
       <Flex align="center" as="a" _hover={{ cursor: "pointer" }} onClick={onClickHome}>
         <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
           ユーザー管理アプリ
         </Heading>
       </Flex>
       <Flex>
-        <Box pr={4} align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
+        <Box 
+          pr={4} 
+          align="center" 
+          fontSize="sm" 
+          flexGrow={2} 
+          display={{ base: "none", md: "flex" }}
+        >
           <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
         </Box>
         <Link onClick={onClickSetting}>設定</Link>
       </Flex>
       <MenuIconButton onOpen={onOpen} />
-    </Flex>
-    <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      </Flex>
+      <MenuDrawer 
+        onClose={onClose} 
+        isOpen={isOpen} 
+        onClickHome={onClickHome} 
+        onClickUserManagement={onClickUserManagement} 
+        onClickSetting={onClickSetting} 
+      />
     </>
   )
 });
