aaeccbab9048481637d1978b7250983898969fd0
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 14:28:18 +0900

    ログイン画面のUIを作成

--- react/src/components/pages/Login.tsx
+++ react/src/components/pages/Login.tsx
@@ -1,5 +1,17 @@
+import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const Login: FC = memo(() => {
-  return <p>ログインページです。</p>
-})
+  return (
+    <Flex align="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
+        <Divider my={4} />
+        <Stack spacing={8} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>ログイン</Button>
+        </Stack>
+      </Box>
+    </Flex>
+  )
+});
