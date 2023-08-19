2f88746931719bfe3e8c4ab117faec1df1008057
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 13:29:22 +0900

    ヘッダーの画面遷移を追加

--- react/src/components/organisms/layout/Header.tsx
+++ react/src/components/organisms/layout/Header.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { FC, memo, useCallback } from "react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useHistory } from "react-router-dom";
 
 export const Header: FC = memo(() => {
 const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const history = useHistory();
+  const onClickHome = useCallback(() => history.push("/home"), []);
+  const onClickUserManagement = useCallback(() => history.push("/home/user_management"), []);
+  const onClickSetting = useCallback(() => history.push("/home/setting"), []);
 
   return (
     <>
@@ -16,14 +23,16 @@ const { isOpen, onOpen, onClose } = useDisclosure();
       justify="space-between" 
       padding={{ base: 3, md: 5 }}
     >
-      <Flex align="center" as="a" _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザー管理アプリ</Heading>
+      <Flex align="center" as="a" _hover={{ cursor: "pointer" }} onClick={onClickHome}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          ユーザー管理アプリ
+        </Heading>
       </Flex>
       <Flex>
         <Box pr={4} align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
-          <Link>ユーザー一覧</Link>
+          <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
         </Box>
-        <Link>設定</Link>
+        <Link onClick={onClickSetting}>設定</Link>
       </Flex>
       <MenuIconButton onOpen={onOpen} />
     </Flex>
