cf1851fead15c0d2807886b702eba44889dbff2d
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 17:32:49 +0900

    ユーザー一覧のユーザーカードをコンポーネント化

--- react/src/components/pages/UserManagement.tsx
+++ react/src/components/pages/UserManagement.tsx
@@ -1,31 +1,13 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { FC, memo } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
   return (
     <Wrap spacing="30px" p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box 
-          w="260px" 
-          h="260px" 
-          bg="white" 
-          borderRadius="10px" 
-          shadow="md" 
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image 
-              borderRadius="full" 
-              boxSize="160px" 
-              src="https://source.unsplash.com/random" 
-              alt="プロフィール画像"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">テスト</Text>
-            <Text fontSize="sm" color="gray">aa</Text>
-          </Stack>
-        </Box>
+        <UserCard imageUrl="https://source.unsplash.com/random" userName="test" fullName="test full name" />
       </WrapItem>
     </Wrap>
   )
