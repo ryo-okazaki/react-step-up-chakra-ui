cb15213a2661f71640213cdbf2848f6a2f4fa7f1
Ryo Okazaki <okazaki3905@gmail.com>
2023-08-19 17:24:00 +0900

    ユーザー一覧のアイコンを作成

--- react/src/components/pages/UserManagement.tsx
+++ react/src/components/pages/UserManagement.tsx
@@ -1,14 +1,32 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const UserManagement: FC = memo(() => {
   return (
-    <Wrap spacing="30px">
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <div style={{ width: "100px",height: "100px", backgroundColor: "teal" }} />
-        </WrapItem>
-      ))}
+    <Wrap spacing="30px" p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box 
+          w="260px" 
+          h="260px" 
+          bg="white" 
+          borderRadius="10px" 
+          shadow="md" 
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image 
+              borderRadius="full" 
+              boxSize="160px" 
+              src="https://source.unsplash.com/random" 
+              alt="プロフィール画像"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">テスト</Text>
+            <Text fontSize="sm" color="gray">aa</Text>
+          </Stack>
+        </Box>
+      </WrapItem>
     </Wrap>
   )
 });
